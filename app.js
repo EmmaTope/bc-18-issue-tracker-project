b64fc0b01006375d43843efb54b5a1f5ad915975
Shaibu Emmanuel <emmanuel.shaibu@andela.com>
2018-08-22 11:46:14 +0100

    add package-lock file

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ require('dotenv').config();
 var config = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID
 };
